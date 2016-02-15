2987880f5b41c2c89154ec60a2202a4607604525
Bogdan470 <R-470@i.ua>
2016-02-15 07:18:38 +0200

    Add

--- GoIT/JavaScript/Lesson_11-12/Shablon/js/script.js
+++ GoIT/JavaScript/Lesson_11-12/Shablon/js/script.js
@@ -1,8 +1,9 @@
 $(function(){
+    
     var html = $('#my_page').html();
     var data = {
        name_foto : "Алтухов Богдан Адександрович", 
-       image_foto_url : "image/foto.png", 
+       image_foto_url : "img/foto.png", 
        img_alt : "Фото Алтухов Богдан", 
        img_title : "Алтухов Богдан Александрович",
        proffesion : "Инженер в сфере аудио-визуальных технологий",
@@ -13,8 +14,7 @@ $(function(){
         would_4 : "Хочу чтоб фронтенд превратился из хобби в основную работу",
         my_tel : "Мой контактный телефон",
         my_number_tel : "+380969419551",
-        text_prof_vkontakte : "Мой профиль вконтакте ",
-        prof_vkontakte : "https://vk.com/id6687563",
+        text_vkontakte : "Мой профиль вконтакте ",
         text_prof_vkontakte : "https://vk.com/id6687563",
         my_feedback : "Мой фидбек: ",
         text_my_feedback : "Готов много учиться и познать новое чтобы быть полезным"
